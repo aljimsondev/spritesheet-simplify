fe777b1f9dcb131f23c3509805f6d34929cd4fc4
aljimsondev <aljimson.megrino@gmail.com>
2022-10-12 11:23:31 +0800

    add some styling

--- src/Components/card/_test.tsx
+++ src/Components/card/_test.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./preview.css";
 import { FaPlay, FaDownload, FaPause } from "react-icons/fa";
 import CreatePreviewThumbnail from "../../renderer/CreatePreviewThumbnail";
 import Loader from "../Loader";
@@ -11,6 +10,8 @@ const config = {
 
 const PreviewCard: React.FC<{
   buffer: HTMLImageElement | undefined;
+  backgroundColor: string;
+  displayBackgroundColor: boolean;
   handleDownload: () => void;
   handlePlayState: (
     sprite: HTMLImageElement,
@@ -18,10 +19,17 @@ const PreviewCard: React.FC<{
     options?: { fps: number }
   ) => void;
   index: number;
-}> = ({ buffer, handleDownload, handlePlayState, index }) => {
+}> = ({
+  buffer,
+  handleDownload,
+  handlePlayState,
+  index,
+  backgroundColor,
+  displayBackgroundColor,
+}) => {
   const [readyState, setReadyState] = React.useState(false);
   const [play, playAnimation] = React.useState(false);
-  const [fps, setFps] = React.useState<number>(5);
+  const [fps, setFps] = React.useState<number>(60);
   const playStateRef = React.useRef<HTMLButtonElement>(null);
   const defaultScreen = { height: 120, width: 120 };
   const canvasWrapperRef = React.useRef<HTMLDivElement>(null);
@@ -56,13 +64,13 @@ const PreviewCard: React.FC<{
 
     const timer = setTimeout(() => {
       setReadyState(true);
-    }, 1000);
+    }, 500);
 
     return () => {
       setReadyState(false);
       clearTimeout(timer);
     };
-  }, []);
+  }, [backgroundColor, displayBackgroundColor]);
 
   const handleChangeFPS = (e: React.ChangeEvent<HTMLInputElement>) => {
     try {
@@ -72,75 +80,77 @@ const PreviewCard: React.FC<{
       console.warn(e);
     }
   };
-
+  //TODO add preview loading in each element
   return (
-    <div className="preview-card">
-      {!readyState && (
-        <div className="preview-loading-base">
-          <p>Loading Preview</p>
-          <Loader />
-        </div>
-      )}
-
-      <React.Fragment>
-        <div
-          ref={canvasWrapperRef}
-          className="preview-canvas-base"
-          id="prev-canvas-base"
-        >
-          <canvas
-            ref={canvasRef}
-            height={defaultScreen.height}
-            width={defaultScreen.width}
-            id="preview-canvas"
-          ></canvas>
-        </div>
-        <div className="preview-controller-base">
-          <div className="form-inline">
-            <div className="fps-input-base">
-              <label>FPS:</label>
-              <p>{fps}</p>
-            </div>
-            <input
-              type="range"
-              value={fps}
-              min={1}
-              max={100}
-              onChange={handleChangeFPS}
-            />
+    <>
+      <div className="preview-card">
+        {!readyState && (
+          <div className="preview-loading-base">
+            <p>Loading Preview</p>
+            <Loader />
           </div>
-          <div className="form-inline">
-            <div className="flex-1">
-              <p ref={frameXTextRef}></p>
-            </div>
-            <div className="flex-1">
-              <p>y: 1</p>
+        )}
+        <React.Fragment>
+          <div ref={canvasWrapperRef} className="prev-canvas-base">
+            <div
+              style={{
+                background: displayBackgroundColor ? backgroundColor : "none",
+              }}
+            >
+              <canvas
+                ref={canvasRef}
+                height={defaultScreen.height}
+                width={defaultScreen.width}
+              ></canvas>
             </div>
           </div>
-          <div className="form-inline">
-            <div className="flex-1">
-              <button
-                type="button"
-                className="btn-preview default"
-                onClick={handleDownload}
-              >
-                <FaDownload />
-              </button>
+          <div className="preview-controller-base">
+            <div className="form-inline">
+              <div className="fps-input-base">
+                <label>FPS:</label>
+                <p>{fps}</p>
+              </div>
+              <input
+                type="range"
+                value={fps}
+                min={1}
+                max={100}
+                onChange={handleChangeFPS}
+              />
             </div>
-            <div className="flex-1">
-              <button
-                type="button"
-                ref={playStateRef}
-                className="btn-preview primary"
-                onClick={handlePlayingState}
-              >
-                {play ? <FaPause /> : <FaPlay />}
-              </button>
+            <div className="form-inline">
+              <div className="flex-1">
+                <p ref={frameXTextRef}></p>
+              </div>
+              <div className="flex-1">
+                <p>y: 1</p>
+              </div>
+            </div>
+            <div className="form-inline">
+              <div className="flex-1">
+                <button
+                  type="button"
+                  className="btn-preview default"
+                  onClick={handleDownload}
+                >
+                  <FaDownload />
+                </button>
+              </div>
+              <div className="flex-1">
+                <button
+                  type="button"
+                  ref={playStateRef}
+                  className="btn-preview primary"
+                  onClick={handlePlayingState}
+                >
+                  {play ? <FaPause /> : <FaPlay />}
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </React.Fragment>
-    </div>
+        </React.Fragment>
+      </div>
+    </>
   );
 };
 
--- src/Components/sidebar/Sidebar.tsx
+++ src/Components/sidebar/Sidebar.tsx
@@ -2,16 +2,32 @@ import React from "react";
 import Tabs from "../Tabs";
 import { Tab } from "../Tabs/Tab";
 import NavMenuForm from "../form/NavMenuForm";
-import "./sidebar.css";
 import PreviewCard from "../card/_test";
 import { Context } from "../../Store/store";
 import Renderer from "../../renderer";
 import Animate from "../../renderer/Animate.test";
 import img from "../../assets/images-removebg-preview.png";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+} from "react-icons/ai";
 
 const Sidebar: React.FC<{}> = () => {
   const { buffers, sidebarRef } = React.useContext(Context);
   const [sprites, setSprites] = React.useState<HTMLImageElement[]>([]);
+  const [backgroundProps, setBackGroundProps] = React.useState<{
+    open: boolean;
+    display: boolean;
+    color: string;
+  }>({
+    open: true,
+    display: true,
+    color: "#BFBFBF",
+  });
+  const [openBg, setOpenBg] = React.useState(false);
+  const [applyColor, setBackgroundColor] = React.useState(false);
   const anim = new Animate();
   // anim.canvasProperties = { height: 120, width: 120 };
   //! FIX ME - animation rendering class
@@ -50,6 +66,22 @@ const Sidebar: React.FC<{}> = () => {
     });
   };
 
+  const handleChangeBgPropsColor = (e: any) => {
+    setBackGroundProps({ ...backgroundProps, [e.target.name]: e.target.value });
+  };
+  const handleClickBgPropsOpen = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setBackGroundProps({ ...backgroundProps, open: !backgroundProps.open });
+  };
+  const handleClickBgPropsDisplay = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setBackGroundProps({
+      ...backgroundProps,
+      display: !backgroundProps.display,
+    });
+  };
   return (
     <div ref={sidebarRef} className="sidebar-base">
       <Tabs>
@@ -64,29 +96,67 @@ const Sidebar: React.FC<{}> = () => {
             )}
             {buffers.length > 0 && (
               <React.Fragment>
-                <div>
-                  <details>
-                    <summary>BACKGROUND</summary>
-                    <div>
-                      <input type="color" />
-                      <label>#DFFFFF</label>
+                <div className="sidebar-bg-details">
+                  <div className="sidebar-bg-summary-base">
+                    <p className="text-title">BACKGROUND</p>
+                    <button
+                      onClick={handleClickBgPropsOpen}
+                      className="-icon-button"
+                    >
+                      {backgroundProps.open ? (
+                        <AiOutlineMinus size={20} />
+                      ) : (
+                        <AiOutlinePlus size={20} />
+                      )}
+                    </button>
+                  </div>
+                  <details open={backgroundProps.open}>
+                    <summary className="sidebase-bg-summary"></summary>
+                    <div className="bg-color-picker-base">
+                      <label
+                        htmlFor="bg-color-picker"
+                        className="bg-color-picker-wrapper"
+                      >
+                        <input
+                          type="color"
+                          value={backgroundProps.color}
+                          onChange={handleChangeBgPropsColor}
+                          name="color"
+                          id="bg-color-picker"
+                        />
+                        <p className="bg-text-color-label">
+                          {backgroundProps.color}
+                        </p>
+                      </label>
+                      <button
+                        onClick={handleClickBgPropsDisplay}
+                        className="-icon-button"
+                      >
+                        {backgroundProps.display ? (
+                          <AiOutlineEye size={20} />
+                        ) : (
+                          <AiOutlineEyeInvisible size={20} />
+                        )}
+                      </button>
                     </div>
                   </details>
                 </div>
-                <div>
-                  <p>ANIMATION PREVIEW</p>
+                <div className="sidebar-anim-preview-base">
+                  <p className="text-title">ANIMATION PREVIEW</p>
+                  {sprites.map((spritesheet, index) => {
+                    return (
+                      <PreviewCard
+                        key={`${spritesheet.dataset.props! + index}`}
+                        index={index}
+                        backgroundColor={backgroundProps.color}
+                        displayBackgroundColor={backgroundProps.display}
+                        buffer={spritesheet}
+                        handleDownload={() => {}}
+                        handlePlayState={handlePlay}
+                      />
+                    );
+                  })}
                 </div>
-                {sprites.map((spritesheet, index) => {
-                  return (
-                    <PreviewCard
-                      key={`${spritesheet.dataset.props! + index}`}
-                      index={index}
-                      buffer={spritesheet}
-                      handleDownload={() => {}}
-                      handlePlayState={handlePlay}
-                    />
-                  );
-                })}
               </React.Fragment>
             )}
           </div>
