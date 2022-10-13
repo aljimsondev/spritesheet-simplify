04a6de0e824534c1f37ad391ce8f047578ab8131
aljimsondev <aljimson.megrino@gmail.com>
2022-10-14 07:11:32 +0800

    add background config

--- src/Components/form/Configuration.tsx
+++ src/Components/form/Configuration.tsx
@@ -3,10 +3,38 @@ import {
   AiOutlinePlus,
   AiOutlineMinus,
   AiOutlineQuestion,
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
 } from "react-icons/ai";
 
 const Configuration = () => {
   const [openBorderOpt, setBorderOpt] = React.useState(true);
+  const [backgroundProps, setBackGroundProps] = React.useState<{
+    open: boolean;
+    display: boolean;
+    color: string;
+  }>({
+    open: true,
+    display: true,
+    color: "#BFBFBF",
+  });
+
+  const handleChangeBgPropsColor = (e: any) => {
+    setBackGroundProps({ ...backgroundProps, [e.target.name]: e.target.value });
+  };
+  const handleClickBgPropsOpen = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setBackGroundProps({ ...backgroundProps, open: !backgroundProps.open });
+  };
+  const handleClickBgPropsDisplay = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setBackGroundProps({
+      ...backgroundProps,
+      display: !backgroundProps.display,
+    });
+  };
 
   const handleToogleBorderOpt = () => {
     setBorderOpt((prevState) => !prevState);
@@ -45,6 +73,46 @@ const Configuration = () => {
           <div className="flex-1 flex items-center">
             <input type="checkbox" className="input-check mr-3" />
             <label className="text-sm">Show borderline</label>
+          </div>
+        </details>
+      </div>
+      <div className="configuration-base outline-b">
+        <div className="flex-1 flex items-center justify-between">
+          <p className="text-title">BACKGROUND</p>
+          <button onClick={handleClickBgPropsOpen} className="-icon-button">
+            {backgroundProps.open ? (
+              <AiOutlineMinus size={20} />
+            ) : (
+              <AiOutlinePlus size={20} />
+            )}
+          </button>
+        </div>
+        <details open={backgroundProps.open}>
+          <summary className="hidden"></summary>
+          <div className="bg-color-picker-base">
+            <label
+              htmlFor="bg-color-picker"
+              className="bg-color-picker-wrapper"
+            >
+              <input
+                type="color"
+                value={backgroundProps.color}
+                onChange={handleChangeBgPropsColor}
+                name="color"
+                id="bg-color-picker"
+              />
+              <p className="bg-text-color-label">{backgroundProps.color}</p>
+            </label>
+            <button
+              onClick={handleClickBgPropsDisplay}
+              className="-icon-button"
+            >
+              {backgroundProps.display ? (
+                <AiOutlineEye size={20} />
+              ) : (
+                <AiOutlineEyeInvisible size={20} />
+              )}
+            </button>
           </div>
         </details>
       </div>
